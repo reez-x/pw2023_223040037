14921a1c8a389e3feb061d1535e120131c783876
reez-x <rizkyabdurrahman455@gmail.com>
2023-02-24 17:31:38 +0700

    Add 2a.php : #tugas2

--- tugas/tugas2/2a.php
+++ tugas/tugas2/2a.php
@@ -1,6 +1,23 @@
 <?php
-$depan="Rizky"
-$belakang="Abdurrahman"
+$depan="Rizky";
+$belakang="Abdurrahman";
+$jml_angka = 100;
+$angkot_rusak = 4;
 
-echo $depan
+
+$no_angka =1;
+while ($no_angka <= $jml_angka ){
+    
+    if ($no_angka %3 == 0)
+    echo "$depan ";
+    if ($no_angka %5 == 0)
+    echo "$belakang <br>";
+    elseif ($no_angka %3 == 0)
+    echo "<br>";
+    
+    else 
+    echo "$no_angka  <br>";
+    $no_angka++;
+}
+
 ?>
